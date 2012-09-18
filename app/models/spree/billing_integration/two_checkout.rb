3fe1a62a3977d0e109e6fc464af00182ece7deb1
Vladimir Kuznetsov <vavaka@gmail.com>
2012-09-19 02:13:38 +0600

    preferred attr_accessors has been added to the two_checkout model

--- app/models/spree/billing_integration/two_checkout.rb
+++ app/models/spree/billing_integration/two_checkout.rb
@@ -3,6 +3,8 @@ class BillingIntegration::TwoCheckout < BillingIntegration
   preference :sid, :string
   preference :secret_word, :string
 
+  attr_accessible :preferred_sid, :preferred_secret_word, :preferred_server, :preferred_test_mode
+
   def provider_class
     ActiveMerchant::Billing::Integrations::TwoCheckout
   end
